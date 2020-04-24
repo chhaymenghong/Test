7d180c17fc1c4f998529c9309ab874a073f17076
chhay menghong <menghong.chhay@jpl.nasa.gov>
2020-04-23 18:13:55 -0700

    a

---
+++ src/app/homePage/home-page/home-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  public onBrandClick() {
+    this.router.navigate(['search'], {
+      queryParams: {
+        brand: 'Samsung'
+      }
+    });
+  }
+
+  public onCategoryClick() {
+    this.router.navigate(['search'], {
+      queryParams: {
+        categories: 'Audio'
+      }
+    });
+  }
+
+}
